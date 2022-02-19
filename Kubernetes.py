3192521538477292adfa31db16844c75579bde27
DickslonKo <91776851+DickslonKo@users.noreply.github.com>
2022-02-19 15:28:18 +0800

    Update Kubernetes.py

--- Kubernetes.py
+++ Kubernetes.py
@@ -7,7 +7,7 @@ from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
     KubernetesPodOperator,
 )
 # Operators; we need this to operate!
-from airflow.operators.bash import BashOperator
+from airflow.operators.bash_operator import BashOperator
 # These args will get passed on to each operator
 # You can override them on a per-task basis during operator initialization
 default_args = {
