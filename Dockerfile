786349b2a0373a6366e2f3a6fcfde0d534b54c85
DickslonKo <91776851+DickslonKo@users.noreply.github.com>
2022-02-19 13:19:45 +0800

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,9 @@
 FROM python:3.9-bullseye
+COPY main.py main.py
 COPY requirements.txt requirements.txt
-RUN pip3 install -r requirements.txt
-COPY . .
+#COPY . .
+RUN pip install -r requirements.txt
+RUN apt-get install -y wget
+RUN wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+RUN apt install -y ./google-chrome-stable_current_amd64.deb
+CMD ["python3", "main.py", "run"]
--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element_by_name("q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
-driver.close()
+s = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=s)
+
+driver.get("https://www.hktvmall.com/")
+
+elem = driver.find_element(By.CSS_SELECTOR, '.SuggestionSearch-input')
+
+elem.send_keys("魚")
+elem.send_keys(Keys.ENTER)
+
+print("done")
